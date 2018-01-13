d310a56f3f1db23925623cb3eed3b785a8186c6a
amiremadz <amire@ucla.edu>
2018-01-13 10:41:06 -0800

    Subsets: iterative.

---
+++ subsets.py
@@ -0,0 +1,52 @@
+# Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+# Note: The solution set must not contain duplicate subsets.
+
+# For example,
+# If nums = [1,2,3], a solution is:
+
+# [
+#   [3],
+#   [1],
+#   [2],
+#   [1,2,3],
+#   [1,3],
+#   [2,3],
+#   [1,2],
+#   []
+# ]
+
+
+class Solution_iterative:
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        subs = [[]]
+        
+        for num in nums:
+            n = len(subs)
+            for idx in range(n):
+                sub = copy.deepcopy(subs[idx])
+                subs.append(sub)
+                subs[-1].append(num)
+        
+        return subs
+
+class Solution_iterative:
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        subs = [[]]
+        
+        for num in nums:
+            subs += [sub + [num] for sub in subs]
+        
+        return subs
+
+
