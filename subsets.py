95e7f6f36818ecb78f3acee243b54c9cfa5d2400
amiremadz <amire@ucla.edu>
2018-01-15 17:14:09 -0800

    Subsets: backtrack.

--- subsets.py
+++ subsets.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 # Given a set of distinct integers, nums, return all possible subsets (the power set).
 
 # Note: The solution set must not contain duplicate subsets.
@@ -67,3 +69,33 @@ class Solution_bitmanip:
                     subs[idx_sub] += [num]
         
         return subs
+
+class Solution_backtrack:
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = [[]]
+        
+        self.helper(nums, 0, [], result)        
+        
+        return result
+    
+    def helper(self, nums, idx, sub, result):
+        for i in range(idx, len(nums)):
+        	# does not work!!
+            # sub += [nums[i]]
+            # result += [sub]
+            # self.helper(nums, i + 1, sub, result)
+            # sub.pop()
+
+            result.append(sub + [nums[i]])
+            self.helper(nums, i + 1, sub + [nums[i]], result)
+
+
+if __name__ == "__main__":
+	s = [1, 2, 3]
+	sol_bktr = Solution_backtrack()
+	subs = sol_bktr.subsets(s)
+	print(subs)
