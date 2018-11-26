abdde7a2e22880450e1f6b1bd11a893d6bae6c8b
Amir Emadzadeh <emadzada@gene.com>
2018-11-26 02:56:42 -0800

    simulate a play

--- ml/Q-learning/frozenlake.py
+++ ml/Q-learning/frozenlake.py
@@ -2,6 +2,9 @@ import numpy as np
 import gym
 import random
 import time
+import os
+
+#from IPython.display import clear_output
 
 # moves: left, right, up, down
 # states:
@@ -40,62 +43,112 @@ max_exploration_rate   = 1
 min_exploration_rate   = 0.01
 exploration_decay_rate = 0.001
 
-rewards_all_episodes = []
+save_name = "q_table"
 
-# Q-learning algorithm
-for episode in range(num_episodes):
-    # initialize new episode params
-    state = env.reset()
-    done = False
-    rewards_current_episode = 0
+if os.path.isfile('q_table.npy'):
+    q_table = np.load(save_name + '.npy')
+else:
+    rewards_all_episodes = []
 
-    if episode % 1000 == 0:
-        print("episode: ", episode)
+    # Q-learning algorithm
+    for episode in range(num_episodes):
+        # initialize new episode params
+        state = env.reset()
+        done = False
+        rewards_current_episode = 0
 
-    for step in range(max_steps_per_episode):
-        # exploration-explotation trade-off
-        exploration_rate_threshold = random.uniform(0, 1)
-        if exploration_rate_threshold > exploration_rate:
-            # choose action via explotation
-            # i.e. chosse action with highest Q-value for currrent state
-            action = np.argmax(q_table[state, :])
-        else:
-            # choose action via exploration
-            # i.e. sample action randomly
-            action = env.action_space.sample()
-        #print("action: ", action)    
+        if episode % 1000 == 0:
+            print("episode: ", episode)
 
-        # take new action
-        new_state, reward, done, info = env.step(action) 
-        #print("info: ", info)
+        for step in range(max_steps_per_episode):
+            # exploration-explotation trade-off
+            exploration_rate_threshold = random.uniform(0, 1)
+            if exploration_rate_threshold > exploration_rate:
+                # choose action via explotation
+                # i.e. chosse action with highest Q-value for currrent state
+                action = np.argmax(q_table[state, :])
+            else:
+                # choose action via exploration
+                # i.e. sample action randomly
+                action = env.action_space.sample()
+           
+            #print("action: ", action)   # 0, 1, 2, 3 
 
-        # update Q-table for Q(s, a)
-        q_table[state, action] = (1 - learning_rate) * q_table[state, action] + learning_rate * (reward + discount_rate * np.max(q_table[new_state, :]))
+            # take new action
+            new_state, reward, done, info = env.step(action) 
+            #print("info: ", info)
 
-        # transition to new state
-        state = new_state
+            # update Q-table for Q(s, a)
+            q_table[state, action] = (1 - learning_rate) * q_table[state, action] + learning_rate * (reward + discount_rate * np.max(q_table[new_state, :]))
 
-        # add new reward
-        rewards_current_episode += reward
+            # transition to new state
+            state = new_state
 
-        if done:
-            break
+            # add new reward
+            rewards_current_episode += reward
 
-    # exploration rate decay
-    exploration_rate = min_exploration_rate + (max_exploration_rate - min_exploration_rate) * np.exp(-exploration_decay_rate * episode)
+            if done:
+                break
 
-    # add current episode reward to total rewards list
-    rewards_all_episodes.append(rewards_current_episode)
+        # exploration rate decay
+        exploration_rate = min_exploration_rate + (max_exploration_rate - min_exploration_rate) * np.exp(-exploration_decay_rate * episode)
 
-# calculate and print average reward per thousand episodes
-num = 1000
-rewards_per_thousand_episodes = np.split(np.array(rewards_all_episodes), num_episodes / num)
+        # add current episode reward to total rewards list
+        rewards_all_episodes.append(rewards_current_episode)
 
-count = num
-print("\n*********Average reward per thousand episodes**********")
-for rewards in rewards_per_thousand_episodes:
-    print(count, ": ", str(sum(rewards / num)))
-    count += num
+    # calculate and print average reward per thousand episodes
+    num = 1000
+    rewards_per_thousand_episodes = np.split(np.array(rewards_all_episodes), num_episodes / num)
+
+    count = num
+    print("\n*********Average reward per thousand episodes**********")
+    for rewards in rewards_per_thousand_episodes:
+        print(count, ": ", str(sum(rewards / num)))
+        count += num
+
+    np.save(save_name, q_table)
 
 print("\n********Updated Q-table*********")
 print(q_table)
+
+# Simulate the game
+
+num_sim_episodes = 3
+
+for episode in range(num_sim_episodes):
+    # initialize new episode params
+    state = env.reset()
+    done = False
+    print("*****EPISODE ", episode+1, " state ", state, " *****")
+    time.sleep(3)
+
+    for step in range(max_steps_per_episode):
+        # show current state of envirnoment on screen
+        #clear_output(wait=True)
+        os.system('clear')
+        env.render()
+        time.sleep(.5)
+
+        # chosse action with highest Q-value for current state
+        action = np.argmax(q_table[state, :])
+
+        # take new action
+        new_state, reward, done, info = env.step(action)
+
+        if done:
+            os.system('clear')
+            env.render()
+            if reward == 1:
+                # agent reached goal and won episode
+                print("***You reached the goal!****")
+            else:
+                # agent stepped in a hole and lost episode
+                print("****You fell throgh a hole!****")
+            time.sleep(3)
+            os.system('clear')
+            break    
+
+        # set new state
+        state = new_state
+
+env.close()     
